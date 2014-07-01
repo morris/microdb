f5f0fd40a70a7e94b2084fc830104ea72f48c065
Morris Brodersen <morrisbrodersen@googlemail.com>
2014-07-01 16:30:13 +0200

    added cache

---
+++ src/MicroDB/Cache.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace MicroDB;
+
+/**
+ * A cache for data loading
+ */
+class Cache {
+	
+	/**
+	 * Constructor
+	 */
+	function __construct($db) {
+		$this->db = $db;
+	}
+	
+	/**
+	 * Load a possibly cached item
+	 */
+	function load($id) {
+		if(is_array($id)) {
+			$results = array();
+			foreach($id as $i) {
+				$results[$i] = $this->load($i);
+			}
+			return $results;
+		}
+		
+		if(isset($this->map[$id])) {
+			return $this->map[$id];
+		} else {
+			$data = $this->db->load($id);
+			$this->map[$id] = $data;
+			return $data;
+		}
+	}
+	
+	/**
+	 * Execute a function on each item (id, data)
+	 */
+	function each($func) {
+		if(!$this->complete) {
+			$this->db->eachId(array($this, 'load'));
+			$this->complete = true;
+		}
+
+		foreach($this->map as $id => $data) {
+			$func($id, $data);
+		}
+	}
+	
+	var $db;
+	var $complete = false;
+	var $map = array();
+}
